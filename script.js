53629486fe67306602689faec4a7cb88e6ad8fc0
ahmed1488013 <ah625634@gmail.com>
2024-11-22 15:28:49 +0300

    gadget
    gadget

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // You can add interactive features here such as hover effects or events
+    const ctaButton = document.querySelector('.cta-button');
+    ctaButton.addEventListener('click', function() {
+        alert('Learn more about Next-Gen Technology!');
+    });
+});
